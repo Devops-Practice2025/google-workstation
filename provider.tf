1760f7ad1dddb9df94abd13cbc6c9f710d63ae4b
karthikeyan <karthikeyangopal16@gmail.com>
2026-03-31 18:39:38 +0200

    test

--- provider.tf
+++ provider.tf
@@ -1,7 +1,8 @@
 provider "google" {
-  project = "orbital-surge-491811-s1" 
+  project = "winged-ratio-491912-c4" 
   # Replace with your actual ID
   region  = "us-east1"
   zone    = "us-east1-b"
-
+  impersonate_service_account = "terraform-sa@winged-ratio-491912-c4.iam.gserviceaccount.com"
 }
+
