5121cadac0936ff16816be87fed05a271851fb04
karthikeyan <karthikeyangopal16@gmail.com>
2026-03-31 02:12:34 +0200

    test

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "google" {
   project = "orbital-surge-491811-s1" 
-  credentials = file("./token.json")# Replace with your actual ID
+  # Replace with your actual ID
   region  = "us-east1"
   zone    = "us-east1-a"
 
